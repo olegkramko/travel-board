ee4c581000e8f1e802473519d5433115e852aa49
OlegKram <kramoleh@gmail.com>
2018-12-02 13:49:15 +0200

    Fix dao

--- src/main/java/web/travelboard/dao/TourDaoImpl.java
+++ src/main/java/web/travelboard/dao/TourDaoImpl.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import web.travelboard.model.Tour;
-
 import java.util.List;
 
 @Repository
